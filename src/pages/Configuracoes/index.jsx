9238e9d0ae4ec93daec20c721726412cee8d7c09
ariany-f <ariany_f@hotmail.com>
2023-11-29 22:44:41 -0300

    cameras e motivos configurações

--- src/pages/Configuracoes/index.jsx
+++ src/pages/Configuracoes/index.jsx
@@ -1,18 +1,51 @@
 import http from '@http'
 import Titulo from '@components/Titulo'
 import Frame from '@components/Frame'
+import DropdownItens from '@components/DropdownItens'
 import { useEffect, useState } from "react"
 
 function Configuracoes(){
 
     const [cameras, setCameras] = useState([])
+    const [motivos, setMotivos] = useState([])
+    const [selectedCamera, setSelectedCamera] = useState('')
+    const [selectedMotivo, setSelectedMotivo] = useState('')
+    const [classError, setClassError] = useState([])
 
     function fetchCameras()
     {
         http.get('api/web/public/cameras')
         .then(response => {
-            setCameras(response)
-            console.log(response);
+            response.map((item) => {
+                let obj = {
+                    name: item.name,
+                    code: item.id
+                }
+                if(!cameras.includes(obj))
+                {
+                    setCameras(estadoAnterior => [...estadoAnterior, obj]);
+                }
+            })
+        })
+        .catch(erro => {
+            console.error(erro)
+        })
+    }
+
+    function fetchMotivos()
+    {
+        http.get('api/web/public/motivos')
+        .then(response => {
+            response.map((item) => {
+                let obj = {
+                    name: item.label,
+                    code: item.id
+                }
+                if(!motivos.includes(obj))
+                {
+                    setMotivos(estadoAnterior => [...estadoAnterior, obj]);
+                }
+            })
         })
         .catch(erro => {
             console.error(erro)
@@ -22,11 +55,8 @@ function Configuracoes(){
     useEffect(() => {
 
         fetchCameras()
+        fetchMotivos()
         
-        const interval = setInterval(() => {
-            fetchCameras()
-          }, 15000);
-          return () => clearInterval(interval);
     }, [])
     
     return (
@@ -35,7 +65,13 @@ function Configuracoes(){
                 <h2 style={{ fontWeight: 500, color: '#B9B9B9' }}>CONFIGURAÇÕES</h2>
             </Titulo>
             <Frame>
-                <h4 style={{ fontWeight: 500, color: '#B9B9B9' }}>TABELA PRINCIPAL</h4>
+                <h4 style={{ fontWeight: 500, color: '#B9B9B9' }}>CÂMERAS E DIREÇÕES</h4>
+
+                 <DropdownItens camposVazios={classError} setValor={setSelectedCamera} valor={selectedCamera} options={cameras} label="CÂMERAS" name="camera" placeholder="" />
+
+                <h4 style={{ fontWeight: 500, color: '#B9B9B9' }}>DESCRIÇÃO DE MOTIVOS DE ERROS</h4>
+                
+                <DropdownItens camposVazios={classError} setValor={setSelectedMotivo} valor={selectedMotivo} options={motivos} label="DESCRIÇÕES" name="reasons" placeholder="" />
             </Frame>
         </>
     )
