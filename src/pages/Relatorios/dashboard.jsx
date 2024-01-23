d5d80331f57e2d4cf9456d1a8dde636699e34b91
ariany-f <ariany_f@hotmail.com>
2024-01-23 13:30:18 -0300

    mostrar porcentagem

--- src/pages/Relatorios/dashboard.jsx
+++ src/pages/Relatorios/dashboard.jsx
@@ -146,7 +146,7 @@ function RelatorioDashboard() {
           type: 'pie',
           data: dataContainer
         });
-        
+
         LimparDatas()
     }, 3000);
 
@@ -206,21 +206,35 @@ function RelatorioDashboard() {
                   <h6>Placa Dianteira</h6>
                 </Titulo>
                 <Chart type="pie" data={dataDianteira} options={configDianteira} className="w-full md:w-10rem" />
-                <p>{dataDianteira.datasets && dataDianteira.datasets[0] ? (parseInt(dataDianteira.datasets[0].data[0]) + parseInt(dataDianteira.datasets[0].data[1])) : ''}</p>
+                <p>{
+                  dataDianteira.datasets && dataDianteira.datasets[0] ?
+                  (Math.floor(parseInt(dataDianteira.datasets[0].data[0]) / ((parseInt(dataDianteira.datasets[0].data[0]) + parseInt(dataDianteira.datasets[0].data[1]))/100)) 
+                  + '%')
+                  : ''}
+                </p>
               </div>
               <div>
                 <Titulo>
                   <h6>Placa Traseira</h6>
                 </Titulo>
                 <Chart type="pie" data={dataTraseira} options={configTraseira} className="w-full md:w-10rem" />
-                <p>{dataTraseira.datasets && dataTraseira.datasets[0] ? (parseInt(dataTraseira.datasets[0].data[0]) + parseInt(dataTraseira.datasets[0].data[1])) : ''}</p>
+                <p>{
+                  dataTraseira.datasets && dataTraseira.datasets[0] ?
+                  (Math.floor(parseInt(dataTraseira.datasets[0].data[0]) / ((parseInt(dataTraseira.datasets[0].data[0]) + parseInt(dataTraseira.datasets[0].data[1]))/100)) 
+                  + '%')
+                  : ''}
+                 </p>
               </div>
               <div>
                 <Titulo>
                   <h6>Conteiner</h6>
                 </Titulo>
                 <Chart type="pie" data={dataContainer} options={configContainer} className="w-full md:w-10rem" />
-                <p>{dataContainer.datasets && dataContainer.datasets[0] ? (parseInt(dataContainer.datasets[0].data[0]) + parseInt(dataContainer.datasets[0].data[1])) : ''}</p>
+                <p>{
+                  dataContainer.datasets && dataContainer.datasets[0] ?
+                  (Math.floor(parseInt(dataContainer.datasets[0].data[0]) / ((parseInt(dataContainer.datasets[0].data[0]) + parseInt(dataContainer.datasets[0].data[1]))/100)) 
+                  + '%')
+                  : ''}</p>
               </div>
           </ContainerLadoALado>
         </>
