8beb9af61055711ea2a6928a45354fdf178bef6b
ariany-f <ariany_f@hotmail.com>
2024-02-16 12:03:12 -0300

    ajuste

--- src/components/DataTablePassagens/index.jsx
+++ src/components/DataTablePassagens/index.jsx
@@ -209,6 +209,7 @@ function DataTablePassagens() {
     const statusBodyTemplate = (rowData) => {
        
         const myArray = rowData.status.split(" | ");
+        console.log(myArray)
         const is_not_ok = myArray.filter((item) => {
             return !item
         })
