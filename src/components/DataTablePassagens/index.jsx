05ae063b526bdbcac14232a858a840ecb6f9882a
ariany-f <ariany_f@hotmail.com>
2024-02-27 16:10:32 -0300

    Ajuste final

--- src/components/DataTablePassagens/index.jsx
+++ src/components/DataTablePassagens/index.jsx
@@ -384,8 +384,6 @@ function DataTablePassagens() {
 
 
     const expandedRowsChange = (e) => {
-        console.log(e)
-        console.log(e.data)
         setExpandedRows(null)
         const open = e.data.slice(-1)
         setExpandedRows(open)
