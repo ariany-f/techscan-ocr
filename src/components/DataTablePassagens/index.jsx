4bdac0865a09dd9d1052293628496a6aaff95184
ariany-f <ariany_f@hotmail.com>
2024-03-04 12:44:49 -0300

    Ajuste

--- src/components/DataTablePassagens/index.jsx
+++ src/components/DataTablePassagens/index.jsx
@@ -325,9 +325,10 @@ function DataTablePassagens() {
                 </ContainerLadoALado>
                 <div>
                     {data.itens.map((passage, index) => {
-                       return {Object.values(passage.images).map((item, index) => {
+                       return Object.values(passage.images).map((item, index) => {
+                            console.log(item)
                             return <img onClick={() => abrirImagem(item)} key={`${data.id}-${index}`} width="240px" src={`http://${window.location.hostname}/api/web/public/${item}`} style={{margin: '5px'}} />
-                        })}
+                        })
                     })}
                 </div>
             </>
