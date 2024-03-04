80046b70b89ce57fc9fc7bb3eebec7ea35d09791
ariany-f <ariany_f@hotmail.com>
2024-03-04 16:28:29 -0300

    Teste

--- src/components/ModalBind/index.jsx
+++ src/components/ModalBind/index.jsx
@@ -103,7 +103,7 @@ function ModalBind({ opened = false, aoClicar, aoFechar, passagem }) {
         {
             const plates = (passagem[0].itens.map(item => {
                 return {
-                    name: item.plate ? `${item.plate} ${item.camera}` : `${item.plate} ${item.camera}`,
+                    name: item.plate ? `Placa: ${item.plate} Camera: ${item.camera} Hora: ${item.datetime}` : `Container: ${item.container} Camera: ${item.camera} Hora: ${item.datetime}`,
                     code: item.id,
                 }
             }))
