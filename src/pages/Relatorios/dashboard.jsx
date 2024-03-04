676e91675f42477157f3cb271531706f6ac52abf
ariany-f <ariany_f@hotmail.com>
2024-03-04 20:20:54 -0300

    console

--- src/pages/Relatorios/dashboard.jsx
+++ src/pages/Relatorios/dashboard.jsx
@@ -295,6 +295,7 @@ function RelatorioDashboard() {
   }
 
   function showLegend() {
+    console.log(dataDianteira)
     setTotalPassagensDianteira(dataDianteira.datasets && dataDianteira.datasets[0] ? parseInt(dataDianteira.datasets[0].data[0]) + parseInt(dataDianteira.datasets[0].data[1]) : 0)
     setTotalPassagensTraseira(dataTraseira.datasets && dataTraseira.datasets[0] ? parseInt(dataTraseira.datasets[0].data[0]) + parseInt(dataTraseira.datasets[0].data[1]) : 0)
     setTotalPassagensContainer(dataContainer.datasets && dataContainer.datasets[0] ? parseInt(dataContainer.datasets[0].data[0]) + parseInt(dataContainer.datasets[0].data[1]) : 0)
